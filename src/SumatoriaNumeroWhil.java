a7e5cb80e27a863b17dd87841f56e0c3f2385f65
lmzea4 <lmzea4@gmail.com>
2024-08-06 12:09:46 -0500

    se crean archivos en la rama main

---
+++ src/SumatoriaNumeroWhil.java
@@ -0,0 +1,11 @@
+public class SumatoriaNumeroWhil{
+    public static void main(String[]arg){
+        int suma = 0;
+        int contador = 1;
+        while (contador <= 10) {
+            suma += contador;
+            contador++;
+            }
+            System.out.println("la sumatoria del 1 al 10 es: " + suma);
+        }
+    }
---
+++ src/SumatoriaNumeros.java
@@ -0,0 +1,10 @@
+public class SumatoriaNumeros {
+    public static void main(String[] arg) {
+        int suma = 0;
+        for (int i = 1; i <= 10; i++) {
+            suma += i;
+        }
+        System.out.println("la sumatoria del 1 al 10 es: " + suma);
+
+    }
+}
